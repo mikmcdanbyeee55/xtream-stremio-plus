a4046444d88a4d83227752d8877175a89b492841
dantheman <80829378+mikmcdanbyeee55@users.noreply.github.com>
2024-02-16 17:03:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ var respond = function (res, data) {
     res.send(data);
   };
   
-// app.use(express.static('public'))
-
 app.engine('html', require('ejs').renderFile);
 app.set('views', __dirname);
 
@@ -63,7 +61,14 @@ app.get('/:userConf/manifest.json', async function (req, res) {
 
 app.get('/:userConf/catalog/:type/:id/:extra?.json', async function (req, res) {
   let {userConf,type,id,extra} = req.params
-  let extraObj
+  let extraObj, userConfiguration
+  
+  // try {
+  //   userConfiguration = JSON.parse(Buffer.from(userConf, 'base64').toString())
+  // } catch (error) {
+  //   console.log(error)
+  //   return []
+  // }
 
   if(extra){
     try {
@@ -78,6 +83,12 @@ app.get('/:userConf/catalog/:type/:id/:extra?.json', async function (req, res) {
     extraObj.genre = extraObj.genre.replace(/\+/g,' ')
   }
 
+  // let pagination
+  // if(extraObj && extraObj.skip){
+  //   pagination =  extra.skip || 1
+  //   pagination = Math.round((Number(extraObj.skip) / 16) + 1 )
+  // }
+  
   let metas = []
   try {
 
@@ -97,6 +108,10 @@ app.get('/:userConf/catalog/:type/:id/:extra?.json', async function (req, res) {
     
   }
     
+  // if(extraObj && extraObj.search){
+  //   metas = await searchCatalogHandler(extraObj.search,userConfiguration.api,pagination,type)
+  // }
+
 });
 
 app.get('/:userConf/meta/:type/:id.json', async function (req,res){
@@ -119,7 +134,6 @@ app.get('/:userConf/meta/:type/:id.json', async function (req,res){
     respond(res,{error})
   }
 });
-
 
 app.get('/:userConf/stream/:type/:id.json', function (req, res) {
 
@@ -145,7 +159,7 @@ app.get('/:userConf/stream/:type/:id.json', function (req, res) {
       description:"Watch Now"
     }]
   }
-
+  // console.log(stream)
   try {
     respond(res,{streams:stream})
   } catch (error) {
