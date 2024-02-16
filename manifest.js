7d4c7af0b5776e19b0d7ac01f7e5d097babd04fc
dantheman <80829378+mikmcdanbyeee55@users.noreply.github.com>
2024-02-16 16:43:05 +0000

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,13 @@
+module.exports={
+    id: "org.community.YourIPTV",
+    version: "1.2.0",
+    name: "Your IPTV",
+    logo: "https://dl.strem.io/addon-logo.png",
+    description: "This addon brings all the Live Streams, VOD streams and Series from your IPTV subscription to your Stremio using Xtream API.",
+    types: ["movie", "series","tv","channel"],
+    background: "https://dl.strem.io/addon-background.jpg",
+    resources: ["movie","series","tv"],
+    catalogs: [],
+    idPrefixes: ["yiptv:"],
+    behaviorHints:{configurable : true, configurationRequired: true },
+};
